89bd93ee5ac12c921c7c84ffe16c4be5e1343ecb
Morgan <morganokeke@gmail.com>
2022-11-21 08:14:01 -0800

    all prototypes

--- main.h
+++ main.h
@@ -1,6 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+* struct identifierStruct - structure definition of a printTypeStruct
+* @indentifier: type
+* @printer: function to print
+*/
+
+typedef struct identifierStruct
+{
+	char *indentifier;
+	int (*printer)(va_list);
+
+} identifierStruct;
+
 int p_ident(char m, va_list arg);
 int _printf(const char *format, ...);
 #endif /*MAIN_H*/
